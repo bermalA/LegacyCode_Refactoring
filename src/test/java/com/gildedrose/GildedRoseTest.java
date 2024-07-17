1232d27901fa89d985af9333ae8774c83856520d
bermalA <aratoglubermal@gmail.com>
2024-07-17 17:02:36 +0300

    Tested project coverage of the tests with kata method part 1

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -19,79 +19,108 @@ class GildedRoseTest {
         assertEquals("foo", item.name);
     }
 
-
     @Test
-    public void systemLowerValue () {
-       Item item = createAndUpdate(15,25, "foo");
-        assertEquals(14, item.sellIn);
-        assertEquals(24, item.quality);
+    public void qualityDecreaseByOne() {
+        Item item = createAndUpdate(1,15, "foo");
+        assertEquals(14, item.quality);
     }
 
     @Test
-    public void qualityDegradesTwiceFast(){
-        Item item = createAndUpdate(0,17, "foo");
+    public void agedBrieQualityIncreseByOne(){
+        Item item = createAndUpdate(0,15, "Aged Brie");
+        assertEquals(17, item.quality);
+    }
+
+    @Test
+    public void BackstagePassQualityLost(){
+        Item item = createAndUpdate(0,15,"Backstage passes to a TAFKAL80ETC concert");
+        assertEquals(0, item.quality);
+    }
+
+    @Test
+    public void SulfurasQualityDontChange(){
+        Item item = createAndUpdate(0,15,"Sulfuras, Hand of Ragnaros");
         assertEquals(15, item.quality);
     }
 
     @Test
-    public void qualityNeverNegative (){
-        Item item = createAndUpdate(0,0, "foo");
-        assertEquals(0, item.quality);
+    public void itemQualityDecreases(){
+        Item item = createAndUpdate(0,10,"foo");
+        assertEquals(8, item.quality);
     }
 
-    @Test
-    public void agedBrieIncreasesQuality(){
-        Item item = createAndUpdate(15,25, "Aged Brie");
-        assertEquals(26, item.quality);
-    }
-
-    @Test
-    public void qualityNever50 () {
-        Item item = createAndUpdate(15,52, "foo");
-        assertEquals(51, item.quality);
-        Item brie = createAndUpdate(0,50, "Aged Brie");
-        assertEquals(50, brie.quality);
-        Item BSPass = createAndUpdate(0,48,"Backstage passes to a TAFKAL80ETC concert");
-        assertEquals(50, BSPass.quality);
-        item = createAndUpdate(1,49, "Backstage passes to a TAFKAL80ETC concert");
-        assertEquals(50, item.quality);
-    }
-
-    @Test
-    public void SulfurasTheLegendary(){
-        Item item = createAndUpdate(1,25, "Sulfuras, Hand of Ragnaros");
-        assertEquals(1, item.sellIn);
-        assertEquals(25, item.quality);
-    }
-
-    @Test
-    public void BackstagePassesIncreaseQuality(){
-        Item item = createAndUpdate(15,25, "Backstage passes to a TAFKAL80ETTE");
-        assertEquals(26, item.quality);
-    }
-
-    @Test
-    public void BackstagePassesIncreaseQuality10Days(){
-        Item item = createAndUpdate(10,25, "Backstage passes to a TAFKAL80ETTE");
-        assertEquals(27, item.quality);
-    }
-
-    @Test
-    public void BackstagePassesIncreaseQuality5Days(){
-        Item item = createAndUpdate(15,25, "Backstage passes to a TAFKAL80ETTE");
-        assertEquals(28, item.quality);
-    }
-
-    @Test
-    public void BackstagePassesQualityOver(){
-        Item item = createAndUpdate(0,25,"Backstage passes to a TAFKAL80ETTE");
-        assertEquals(0, item.quality);
-    }
-
-    @Test
-    public void BrieNeverExpires (){
-        Item item = createAndUpdate(0,25,"Aged Brie");
-        assertEquals(-1, item.sellIn);
-        assertEquals(27, item.quality);
-    }
+//    @Test
+//    public void systemLowerValue () {
+//       Item item = createAndUpdate(15,25, "foo");
+//        assertEquals(14, item.sellIn);
+//        assertEquals(24, item.quality);
+//    }
+//
+//    @Test
+//    public void qualityDegradesTwiceFast(){
+//        Item item = createAndUpdate(0,17, "foo");
+//        assertEquals(15, item.quality);
+//    }
+//
+//    @Test
+//    public void qualityNeverNegative (){
+//        Item item = createAndUpdate(0,0, "foo");
+//        assertEquals(0, item.quality);
+//    }
+//
+//    @Test
+//    public void agedBrieIncreasesQuality(){
+//        Item item = createAndUpdate(15,25, "Aged Brie");
+//        assertEquals(26, item.quality);
+//    }
+//
+//    @Test
+//    public void qualityNever50 () {
+//        Item item = createAndUpdate(15,52, "foo");
+//        assertEquals(51, item.quality);
+//        Item brie = createAndUpdate(0,50, "Aged Brie");
+//        assertEquals(50, brie.quality);
+//        Item BSPass = createAndUpdate(0,48,"Backstage passes to a TAFKAL80ETC concert");
+//        assertEquals(50, BSPass.quality);
+//        item = createAndUpdate(1,49, "Backstage passes to a TAFKAL80ETC concert");
+//        assertEquals(50, item.quality);
+//    }
+//
+//    @Test
+//    public void SulfurasTheLegendary(){
+//        Item item = createAndUpdate(1,25, "Sulfuras, Hand of Ragnaros");
+//        assertEquals(1, item.sellIn);
+//        assertEquals(25, item.quality);
+//    }
+//
+//    @Test
+//    public void BackstagePassesIncreaseQuality(){
+//        Item item = createAndUpdate(15,25, "Backstage passes to a TAFKAL80ETTE");
+//        assertEquals(26, item.quality);
+//    }
+//
+//    @Test
+//    public void BackstagePassesIncreaseQuality10Days(){
+//        Item item = createAndUpdate(10,25, "Backstage passes to a TAFKAL80ETTE");
+//        assertEquals(27, item.quality);
+//    }
+//
+//    @Test
+//    public void BackstagePassesIncreaseQuality5Days(){
+//        Item item = createAndUpdate(15,25, "Backstage passes to a TAFKAL80ETTE");
+//        assertEquals(28, item.quality);
+//    }
+//
+//    @Test
+//    public void BackstagePassesQualityOver(){
+//        Item item = createAndUpdate(0,25,"Backstage passes to a TAFKAL80ETTE");
+//        assertEquals(0, item.quality);
+//    }
+//
+//    @Test
+//    public void agedBrieNeverExpires (){
+//        Item item = createAndUpdate(0,25,"Aged Brie");
+//        assertEquals(-1, item.sellIn);
+//        assertEquals(27, item.quality);
+//    }
 }
